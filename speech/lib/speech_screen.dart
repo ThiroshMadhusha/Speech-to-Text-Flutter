03eee824903b191c70d0949523dec61225551a1a
Jayathunga T.M <thiroshmadhusha0520@gmail.com>
2023-08-24 07:09:36 +0530

    create main hold voice action button

---
+++ speech/lib/speech_screen.dart
@@ -0,0 +1,63 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+class SpeechScreen extends StatefulWidget {
+  const SpeechScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SpeechScreen> createState() => _SpeechScreenState();
+}
+
+class _SpeechScreenState extends State<SpeechScreen> {
+
+var text = "Hold the button to speak";
+var isListening = false;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      // action button on bottom
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+        endRadius: 75.0,
+        animate: isListening,
+        duration: const Duration(milliseconds: 2000),
+        glowColor: bgColor,
+        repeat: true,
+        repeatPauseDuration: const Duration(milliseconds: 100),
+        showTwoGlows: true,
+        child: const CircleAvatar(
+          backgroundColor: bgColor,
+          radius: 35,
+          child: Icon(Icons.mic, color: Colors.white),
+        )
+      ),
+
+      // navigation bar
+      appBar: AppBar(
+        leading: const Icon(Icons.sort_rounded, color: Colors.white),
+        centerTitle: true,
+        backgroundColor: bgColor,
+        elevation: 0.0,
+        title: const Text('Voice Assistant', style:TextStyle(
+          color: textColor,
+        )),
+      ),
+
+      // body of the screen 
+      body: Container(
+        // color: Colors.red,
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        // margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        margin: const EdgeInsets.only(bottom: 150, left: 16, right: 16, top: 16),
+        child: Text(text, 
+        style: const TextStyle(fontSize: 24, color: Colors.black54, fontWeight: FontWeight.w600),
+        ),
+      ),
+    );
+  }
+}
