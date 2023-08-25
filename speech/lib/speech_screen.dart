bff23c7a09aa6daf27a2acaf659732cd083a935e
Jayathunga T.M <thiroshmadhusha0520@gmail.com>
2023-08-25 23:59:53 +0530

    complete speech to text ui design

--- speech/lib/speech_screen.dart
+++ speech/lib/speech_screen.dart
@@ -1,6 +1,6 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
-
+import 'package:speech_to_text/speech_to_text.dart';
 import 'colors.dart';
 
 class SpeechScreen extends StatefulWidget {
@@ -11,48 +11,60 @@ class SpeechScreen extends StatefulWidget {
 }
 
 class _SpeechScreenState extends State<SpeechScreen> {
+  SpeechToText speechToText = SpeechToText();
 
-var text = "Hold the button to speak";
-var isListening = false;
-  
+  var text = "Hold the Button to Speak";
+  var isListening = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       // action button on bottom
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: AvatarGlow(
-        endRadius: 75.0,
-        animate: isListening,
-        duration: const Duration(milliseconds: 2000),
-        glowColor: bgColor,
-        repeat: true,
-        repeatPauseDuration: const Duration(milliseconds: 100),
-        showTwoGlows: true,
+          endRadius: 75.0,
+          animate: isListening,
+          duration: const Duration(milliseconds: 2000),
+          glowColor: bgColor,
+          repeat: true,
+          repeatPauseDuration: const Duration(milliseconds: 100),
+          showTwoGlows: true,
 
-        // GestureDetector is a widget that detects gestures
-        child: GestureDetector(
-          onTapDown:(details) {
-            setState(() {
-              isListening = true;
-              text = "Listening...";
-            });
-          },
-          onTapUp: (details) {
-            setState(() {
-              isListening = false;
-              text = "Hold the button to speak";
-            });
-          },
-          
-          // CircleAvatar is a circular widget with background color and radius
-          child: const CircleAvatar(
-            backgroundColor: bgColor,
-            radius: 35,
-            child: Icon(Icons.mic, color: Colors.white),
-          ),
-        )
-      ),
+          // GestureDetector is a widget that detects gestures
+          child: GestureDetector(
+            onTapDown: (details) async {
+              if (!isListening) {
+                var available = await speechToText.initialize();
+                if (available) {
+                  setState(() {
+                    isListening = true;
+                    speechToText.listen(
+                      onResult: (result) {
+                        setState(() {
+                          text = result.recognizedWords;
+                        });
+                      },
+                    );
+                  });
+                }
+              }
+            },
+
+            onTapUp: (details) {
+              setState(() {
+                isListening = false;
+              });
+              speechToText.stop();
+            },
+
+            // CircleAvatar is a circular widget with background color and radius
+            child: CircleAvatar(
+              backgroundColor: bgColor,
+              radius: 35,
+              child: Icon(isListening ? Icons.mic : Icons.mic_none,
+                  color: Colors.white),
+            ),
+          )),
 
       // navigation bar
       appBar: AppBar(
@@ -60,20 +72,32 @@ var isListening = false;
         centerTitle: true,
         backgroundColor: bgColor,
         elevation: 0.0,
-        title: const Text('Voice Assistant', style:TextStyle(
-          color: textColor,
-        )),
+        title: const Text('Speech To Text',
+            style: TextStyle(
+              color: textColor,
+            )),
       ),
 
-      // body of the screen 
-      body: Container(
-        // color: Colors.red,
-        alignment: Alignment.center,
-        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-        // margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-        margin: const EdgeInsets.only(bottom: 150, left: 16, right: 16, top: 16),
-        child: Text(text, 
-        style: const TextStyle(fontSize: 24, color: Colors.black54, fontWeight: FontWeight.w600),
+      // body of the screen
+      body: SingleChildScrollView(
+        reverse: true,
+        physics: const BouncingScrollPhysics(),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.7,
+          // color: Colors.red,
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          // margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          margin:
+              const EdgeInsets.only(bottom: 150, left: 16, right: 16, top: 16),
+          child: Text(
+            text,
+            style: TextStyle(
+                fontSize: 24,
+                color: isListening ? Colors.black87 : Colors.black54,
+                fontWeight: FontWeight.w600),
+          ),
         ),
       ),
     );
